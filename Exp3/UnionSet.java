29c6760679fe386543e48701ab19b718b8a3e9bc
SVTA-OP <saravana.senthil64@outlook.com>
2025-09-04 11:13:34 +0530

    Half finished Exp3 q2

--- Exp3/UnionSet.java
+++ Exp3/UnionSet.java
@@ -1,111 +1,47 @@
-interface SetADT {
+interface SetADT{
     boolean add(int item);
     boolean remove(int item);
-    boolean contains(int item);
+    boolean contains (int item);
     SetADT union(SetADT otherSet);
 }
 
 class IntSet implements SetADT {
-    int item;
-    public boolean add(int item) { return true; }
-    public boolean remove(int item) { return true; }
-    public boolean contains(int item) { return true; }
-    public SetADT union(SetADT otherSet) { return null; }
-}
-
-class OrderedIntSet implements SetADT {
-    SetADT orderedSet;
-    public boolean add(int item) { return true; }
-    public boolean remove(int item) { return true; }
-    public boolean contains(int item) { return true; }
-    public SetADT union(SetADT otherSet) { return null; }
-}
-
-public class UnionSet implements SetADT {
-    private IntSet set;
-
-    public UnionSet() {
-        set = new IntSet();
+    int[] elements;
+    int size = 0;
+    
+    public boolean add(int e){
+        elements[size++] = e;
+        return true;
     }
-
-    public boolean add(int item) {
-        return set.add(item);
-    }
-
-    public boolean remove(int item) {
-        return set.remove(item);
-    }
-
-    public boolean contains(int item) {
-        return set.contains(item);
-    }
-
-    public SetADT union(SetADT otherSet) {
-        UnionSet result = new UnionSet();
-        for (int item : ((IntSet) set).getItems()) {
-            result.add(item);
-        }
-        for (int item : ((IntSet) otherSet).getItems()) {
-            result.add(item);
-        }
-        return result;
-    }
-}
-
-class IntSet implements SetADT {
-    private int[] items;
-    private int size;
-    private static final int MAX_SIZE = 100;
-
-    public IntSet() {
-        items = new int[MAX_SIZE];
-        size = 0;
-    }
-
-    public boolean add(int item) {
-        if (!contains(item) && size < MAX_SIZE) {
-            items[size++] = item;
-            return true;
-        }
-        return false;
-    }
-
-    public boolean remove(int item) {
-        for (int i = 0; i < size; i++) {
-            if (items[i] == item) {
-                for (int j = i; j < size - 1; j++) {
-                    items[j] = items[j + 1];
+    
+    public boolean remove(int item){
+        boolean flag = false;
+        if (size == 0)
+            return false;
+        else{
+            for (int i = 0; i<size; i++){
+                if (elements[i] == item){
+                    flag = true;
                 }
-                size--;
-                return true;
+                if (flag == true){
+                    elements[i] = elements[i+1];
+                }
             }
+            if (flag == true)
+                return true;
+            else
+            return false;
+        }
+    }
+
+    public boolean contains(int item){
+        for (int i=0; i<size; i++){
+            if (elements[i] == item)
+                return true;
         }
         return false;
     }
 
-    public boolean contains(int item) {
-        for (int i = 0; i < size; i++) {
-            if (items[i] == item) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public SetADT union(SetADT otherSet) {
-        UnionSet result = new UnionSet();
-        for (int item : getItems()) {
-            result.add(item);
-        }
-        for (int item : ((IntSet) otherSet).getItems()) {
-            result.add(item);
-        }
-        return result;
-    }
-
-    public int[] getItems() {
-        int[] result = new int[size];
-        System.arraycopy(items, 0, result, 0, size);
-        return result;
+    SetADT union(SetADT otherSet){
     }
 }
